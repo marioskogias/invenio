39d501704b7872c535463523a85c4464708dfb0f
Marios Kogias <marioskogias@gmail.com>
2014-11-27 10:57:30 +0100

    Add first simple mapping

--- invenio/ext/elasticsearch/config/mapping.py
+++ invenio/ext/elasticsearch/config/mapping.py
@@ -1,6 +1,22 @@
 mappings = {
     "records": {
         "properties": {
+            "files": {
+                "type": "nested"
+            },
+            "title": {
+                "type": "multi_field",
+                "fields": {
+                    "title": {
+                        "type": "string",
+                        "index": "analyzed"
+                    },
+                    "raw": {
+                        "type": "string",
+                        "index": "not_analyzed"
+                    }
+                }
+            },
             "collections": {
                 "index": "not_analyzed",
                 "type": "string"
@@ -9,13 +25,87 @@ mappings = {
                 "type": "nested",
                 "properties": {
                     "first_name": {
-                        "type": "string"
+                        "type": "multi_field",
+                        "fields": {
+                            "first_name": {
+                                "type": "string",
+                                "index": "analyzed"
+                            },
+                            "raw": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            }
+                        }
                     },
                     "last_name": {
-                        "type": "string"
+                        "type": "multi_field",
+                        "fields": {
+                            "last_name": {
+                                "type": "string",
+                                "index": "analyzed"
+                            },
+                            "raw": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            }
+                        },
+                        "full_name": {
+                            "type": "multi_field",
+                            "fields": {
+                                "full_name": {
+                                    "type": "string",
+                                    "index": "analyzed"
+                                },
+                                "raw": {
+                                    "type": "string",
+                                    "index": "not_analyzed"
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            "_first_author": {
+                "type": "nested",
+                "properties": {
+                    "first_name": {
+                        "type": "multi_field",
+                        "fields": {
+                            "first_name": {
+                                "type": "string",
+                                "index": "analyzed"
+                            },
+                            "raw": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            }
+                        }
                     },
-                    "full_name": {
-                        "type": "string"
+                    "last_name": {
+                        "type": "multi_field",
+                        "fields": {
+                            "last_name": {
+                                "type": "string",
+                                "index": "analyzed"
+                            },
+                            "raw": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            }
+                        },
+                        "full_name": {
+                            "type": "multi_field",
+                            "fields": {
+                                "full_name": {
+                                    "type": "string",
+                                    "index": "analyzed"
+                                },
+                                "raw": {
+                                    "type": "string",
+                                    "index": "not_analyzed"
+                                }
+                            }
+                        }
                     }
                 }
             }
