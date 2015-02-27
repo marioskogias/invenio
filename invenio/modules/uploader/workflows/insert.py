e1c48b0aebb4f33237a22a6ba3ee542ff34c8583
Marios Kogias <marioskogias@gmail.com>
2015-02-27 11:05:47 +0100

    Add modified workflow

--- invenio/modules/uploader/workflows/insert.py
+++ invenio/modules/uploader/workflows/insert.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-# Copyright (C) 2014 CERN.
-#
-# Invenio is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# Invenio is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Invenio; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+##
+## This file is part of Invenio.
+## Copyright (C) 2014 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """Default workflows for insert records using the uploader.
 
@@ -37,6 +37,7 @@ from invenio.modules.uploader.uploader_tasks import \
     save_master_format, \
     save_record, \
     update_pidstore,\
+    elasticsearch_upload,\
     validate
 
 
@@ -63,13 +64,15 @@ class insert(object):
                        )
             ]
         ),
+        #elasticsearch_upload(step=2),
         reserve_record_id(step=2),
         validate(step=3),
-        manage_attached_documents(step=4),
-        save_record(step=5),
-        update_pidstore(step=6),
-        save_master_format(step=7),
-        legacy(step=8),
+        #manage_attached_documents(step=4),
+        save_record(step=3),
+        update_pidstore(step=4),
+        elasticsearch_upload(step=5)
+        #save_master_format(step=7),
+        #legacy(step=8),
     ]
 
     post_tasks = [
